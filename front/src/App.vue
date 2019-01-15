71d4c62411e2fa196c0b1bb4fc87d393431fc5a0
Nini Mihaila <ninimihaila@users.noreply.github.com>
2019-01-15 17:22:51 +0200

    Init

---
+++ front/src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <div id="app">
+    <Upload />
+  </div>
+</template>
+
+<script>
+import 'bulma/css/bulma.css'
+import '@fortawesome/fontawesome-free/css/all.css'
+import '@fortawesome/fontawesome-free/js/all.js'
+import Upload from './components/Upload.vue'
+
+export default {
+  name: 'app',
+  components: {
+    Upload
+  }
+}
+</script>
+
+<style>
+</style>
---
+++ front/src/components/Upload.vue
@@ -0,0 +1,79 @@
+<template>
+  <form @submit.prevent="sendFile" enctype="multipart/form-data">
+
+    <div class="field">
+      <div class="file is-boxed is-primary">
+        <label class="file-label">
+          <input
+            type="file"
+            ref="files"
+            class="file-input"
+            @change="selectFiles"
+          />
+
+          <span class="file-cta">
+            <span class="file-icon">
+              <i class="fas fa-upload"></i>
+            </span>
+            <span class="file-label">
+              Upload
+            </span>
+          </span>
+
+          <span v-if="files" class="file-name">{{ files.name }}</span>
+
+        </label>
+      </div>
+
+    </div>
+
+    <div class="field">
+      <button class="button is-info">
+        Send
+      </button>
+    </div>
+
+  </form>
+</template>
+
+<script>
+export default {
+  name: "Upload",
+
+  data() {
+    return {
+      files: "",
+    }
+  },
+
+  methods: {
+    selectFiles() {
+      this.files = this.$refs.files.files[0];
+    },
+
+    async sendFile() {
+      const formData = new FormData();
+      formData.append('file', this.files)
+
+      try {
+        await fetch('/upload', {
+          method: 'post',
+          body: formData,
+        })
+      } catch(err) {
+        // eslint-disable-next-line
+        console.log(err)
+      }
+    }
+  },
+
+  // props: {
+  //   msg: String
+  // }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
